9a7a045e5b87d290b0427f3889dcad457932ef06
HenryS <18201042@ittelkom-pwt.ac.id>
2024-05-31 20:25:08 +0000

    fs/aio: Fix .345 Issue
    Signed-off-by: HenryS <18201042@ittelkom-pwt.ac.id>

--- fs/aio.c
+++ fs/aio.c
@@ -566,10 +566,7 @@ void kiocb_set_cancel_fn(struct kiocb *iocb, kiocb_cancel_fn *cancel)
 	if (WARN_ON_ONCE(!list_empty(&req->ki_list)))
 		return;
 
-	req = container_of(iocb, struct aio_kiocb, common);
-	ctx = req->ki_ctx;
-
-	spin_lock_irqsave(&ctx->ctx_lock, flags);
+		spin_lock_irqsave(&ctx->ctx_lock, flags);
 	list_add_tail(&req->ki_list, &ctx->active_reqs);
 	req->ki_cancel = cancel;
 	spin_unlock_irqrestore(&ctx->ctx_lock, flags);
